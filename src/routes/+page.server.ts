8bb2df7df80f147cd620fcf1d878ae72673562ef
Jazza231 <greeny-231@outlook.com>
2025-01-03 00:29:49 +0800

    Today I found out that you can cache the results from +page.server.ts files, thank you paolo

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,13 @@
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({ setHeaders }) => {
    const today = Date.now();
    const birthday = Date.parse("2006-04-07");
    const age = new Date(today - birthday).getFullYear() - 1970;
+
+   setHeaders({
+      "cache-control": "public, max-age=3600",
+   });
 
    return { age };
 };
